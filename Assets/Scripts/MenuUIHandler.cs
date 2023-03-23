4f20b9e0eb94c4800563fd9155f9b2d13fe16ab8
lpschi <smith950@gctcok.edu>
2023-03-23 08:50:20 -0500

    Initial commit

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public ColorPicker ColorPicker;
+    private Color color;
+
+    public void NewColorSelected(Color color)
+    {
+        // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
+    }
+
+    private void Start()
+    {
+        ColorPicker.Init();
+        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    { 
+        MainManager.Instance.SaveColor(); 
+#if (UNITY_EDITOR)
+        
+            EditorApplication.ExitPlaymode();
+#else
+#endif
+    }
+    public void NewColorSelected()
+    {
+        MainManager.Instance.TeamColor = color;
+    }
+    private static void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+public void SaveColorClicked()
+{ 
+    MainManager.Instance.SaveColor();
+}
+
+public void LoadColorClicked()
+{
+    MainManager.Instance.LoadColor();
+    ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+}
+}
+
+
